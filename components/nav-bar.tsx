229feb884840a09cd3f84ff1a2a2fd75fa991a15
Rich Joshua Alzate <josh.alzate17@gmail.com>
2022-04-24 22:18:28 +0800

    Added home navigation when the Head Title is clicked

--- components/nav-bar.tsx
+++ components/nav-bar.tsx
@@ -1,11 +1,14 @@
 import { Button, Flex, Heading, useColorMode } from "@chakra-ui/react";
 import { MoonIcon, SunIcon, } from '@chakra-ui/icons';
+import Link from "next/link";
 
 const NavBar = () => {
     const { colorMode, toggleColorMode } = useColorMode();
     return (
         <Flex borderBottom={'1px'} borderColor={'gray.200'} py={5} mb={5}>
-            <Heading as="h1" size={'lg'}>Mancala</Heading>
+            <Link href={'/'}>
+                <Heading as="h1" size={'lg'} cursor={'pointer'}>Mancala</Heading>
+            </Link>
             <Button onClick={toggleColorMode} ml={'auto'}>
                 {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
             </Button>
