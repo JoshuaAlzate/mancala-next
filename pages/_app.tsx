51208f283316078ee7a5086e21113c50dc3ff426
Rich Joshua Alzate <josh.alzate17@gmail.com>
2022-04-25 13:12:17 +0800

    Include global template

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,18 @@
 import { NextPage } from 'next';
-import { ChakraProvider } from '@chakra-ui/react';
+import { Box, ChakraProvider } from '@chakra-ui/react';
 import colorTheme from '../theme';
 import '../styles/globals.scss';
 
 const MyApp = ({ Component, pageProps }: { Component: NextPage, pageProps: any }) => {
   return (
     <ChakraProvider theme={colorTheme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+      <Box mx={'auto'}
+        width={'100%'} maxW={{ base: 'unset', lg: '1200px' }}
+        minH={'100%'}
+        px={5}>
+        <Component {...pageProps} />
+      </Box>
+    </ChakraProvider >
   );
 }
 
