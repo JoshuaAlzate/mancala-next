1e892aaef2aa184c758dc5533dab032f5059fe46
Rich Joshua Alzate <josh.alzate17@gmail.com>
2022-04-27 10:18:32 +0800

    Added Room ID Page

---
+++ pages/room/[id].tsx
@@ -0,0 +1,95 @@
+import { Button, Flex, Heading } from "@chakra-ui/react";
+import NavBar from "components/nav-bar";
+import { PlayerItemProps } from "components/player-item";
+import PlayerList from "components/player-list";
+import { RoomItemProps } from "components/room-item";
+import useSockClient from "hooks/useSockClient";
+import { NextPage } from "next";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
+const RoomTokenPage: NextPage = (room: RoomItemProps) => {
+    const [player, setPlayer] = useState<PlayerItemProps>();
+    const [sessionRoom, setSessionRoom] = useState<RoomItemProps>(room);
+
+    const roomSubject = useSockClient(`/topic/room-update/${room.id}`);
+
+    const router = useRouter();
+    const { id } = router.query;
+
+    useEffect(() => {
+        window.sessionStorage.setItem('currentRoom', JSON.stringify(sessionRoom));
+    }, [sessionRoom])
+
+    useEffect(() => {
+        const currentPlayer = window.sessionStorage.getItem('currentPlayer');
+        if (currentPlayer) {
+            const sessionPlayerDetails = JSON.parse(currentPlayer);
+            setPlayer(sessionPlayerDetails);
+        }
+    }, []);
+
+    useEffect(() => {
+        roomSubject?.subscribe((data: RoomItemProps[]) => {
+            if (data) setSessionRoom(prevValue => {
+                return {
+                    ...prevValue,
+                    ...data
+                }
+            });
+        });
+
+        return () => {
+            roomSubject?.unsubscribe();
+        }
+    }, [roomSubject])
+
+    useEffect(() => {
+        router.beforePopState(({ as }) => {
+            if (as !== router.asPath) {
+                window.sessionStorage.removeItem('currentRoom');
+                exitRoom(player?.id);
+            }
+            return true;
+        });
+
+        return () => {
+            router.beforePopState(() => true);
+        };
+    }, [router, player]);
+
+    const exitRoom = async (playerID: string | undefined) => {
+        return await fetch(`${process.env.NEXT_PUBLIC_HOST}/room/checkRoomPlayers`, {
+            body: JSON.stringify({
+                roomID: id,
+                playerID
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            method: 'POST'
+        });
+    }
+
+    return (
+        <>
+            <NavBar />
+            <Flex>
+                <Heading as={'h2'} size={'md'}>Waiting for opponent</Heading>
+                <Button onClick={router.back} colorScheme={'red'} ml={'auto'}>Exit Room</Button>
+            </Flex>
+            <PlayerList firstPlayer={sessionRoom.firstPlayer} secondPlayer={sessionRoom.secondPlayer} />
+
+        </>
+    )
+}
+
+RoomTokenPage.getInitialProps = async ({ query }) => {
+    const { id } = query;
+    const result = await fetch(`${process.env.NEXT_PUBLIC_HOST}/room/getDetails/${id}`);
+    const room = await (await result.json() as Promise<RoomItemProps>);
+    return room;
+}
+
+
+export default RoomTokenPage;
